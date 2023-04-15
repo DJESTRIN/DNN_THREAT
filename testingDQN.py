49536414e5c73f52e0f916d10ba662af2d53fd38
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-04-14 21:55:23 -0400

    Update testingDQN.py

--- testingDQN.py
+++ testingDQN.py
@@ -51,10 +51,10 @@ class DQN(nn.Module):
 
     def __init__(self, n_observations, n_actions):
         super(DQN, self).__init__()
-        self.layer1 = nn.Linear(n_observations, 10000)
-        self.layer2 = nn.Linear(10000, 1000)
-        self.layer3 = nn.Linear(1000, 500)
-        self.layer4 = nn.Linear(500, 128)
+        self.layer1 = nn.Linear(n_observations, 1000)
+        self.layer2 = nn.Linear(1000, 100)
+        self.layer3 = nn.Linear(100, 10)
+        self.layer4 = nn.Linear(10, 128)
         self.layer5 = nn.Linear(128, 128)
         self.layer6 = nn.Linear(128, n_actions)
 
