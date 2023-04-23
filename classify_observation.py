4ca0f949398f246c94c75feff9b90eed806c2345
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-04-23 14:25:06 -0400

    Create classify_observation.py

---
+++ classify_observation.py
@@ -0,0 +1,26 @@
+import ilastik
+import numpy as np
+
+class classify_observation(object):
+  def __init__(self,img_array,step,episode):
+    #input into ilastik
+    self.forward()
+    
+    def forward():
+      #Common sequence of functions
+    
+    def get_common_objects(self):
+      #Get agent location
+      #Get enemy (number + location)
+      
+    def write_data(self):
+     #save important data for analysis. 
+      
+class BattleZone(classify_observation):
+  def forward(self):
+    self.get_common_objects(self)
+    self.addtional_objects(self)
+    self.write_data(self)
+    
+  def additional_objects(self)
+  
