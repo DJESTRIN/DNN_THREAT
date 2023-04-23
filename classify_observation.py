e794a289a9f75b44ee4d7415ccb233cdcf05bc7d
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-04-23 15:54:31 -0400

    Update classify_observation.py

--- classify_observation.py
+++ classify_observation.py
@@ -1,13 +1,13 @@
 import ilastik
 import numpy as np
+import subprocess 
 
 class classify_observation(object):
   def __init__(self,img_array,step,episode):
     #input into ilastik
-    self.forward()
-    
-    def forward():
-      #Common sequence of functions
+    self.get_common_objects()
+    self.additiona_objects()
+    self.write_data()
     
     def get_common_objects(self):
       #Get agent location
@@ -15,12 +15,15 @@ class classify_observation(object):
       
     def write_data(self):
      #save important data for analysis. 
+    
+    def additiona_objects(self):
+      return 
       
 class BattleZone(classify_observation):
-  def forward(self):
-    self.get_common_objects(self)
-    self.addtional_objects(self)
-    self.write_data(self)
-    
   def additional_objects(self)
+  
+  
+class SpaceInvaders(classify_observation):
+  def additional_objects(self)
+  
   
