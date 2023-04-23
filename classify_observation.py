0312fedfa1dd1569a78da6204ed1292f8e1a23bf
anthonypoon12 <anthony@poons.us>
2023-04-23 18:08:31 -0400

    testing different methods of using ilastik through python

--- classify_observation.py
+++ classify_observation.py
@@ -1,26 +1,52 @@
-import ilastik
-import numpy as np
+import subprocess
 
-class classify_observation(object):
-  def __init__(self,img_array,step,episode):
-    #input into ilastik
-    self.forward()
-    
-    def forward():
-      #Common sequence of functions
-    
-    def get_common_objects(self):
-      #Get agent location
-      #Get enemy (number + location)
+# for simple commands
+subprocess.run(["C:\Program Files\ilastik-1.4.0\ilastik.exe --headless --project=BattleZoneVersion0.ilp BattleZoneImageFolder\BattleZone-v4Image33.png BattleZoneImageFolder\BattleZone-v4Image150.png"], shell=True) 
+
+path = "C:\Program Files\ilastik-1.4.0"
+
+# cd "\Program Files\ilastik-1.3.2"
+# $ .\ilastik.exe --headless --project=MyProject.ilp my_next_image1.png my_next_image2.png
+
+# from ilastik.applets.dataSelection.opDataSelection import DatasetInfo
+# from ilastik.workflows.pixelClassification import PixelClassificationWorkflow
+# import numpy as np
+
+# class classify_observation:
+#   def __init__(self,img_array,step,episode):
+#     #input into ilastik
+#     # Load input data
+#     self.datapath = "Battle Zone Image Folder"
+#     self.input_data = ilastik.DatasetPath(self.datapath)
+#     # Load ilastik project
+#     self.ilastikprojectfile = "SpaceInvadersVersion0.ilp"
+#     self.ilastik_project = ilastik.ProjectFile(self.ilastikprojectfile)
+#     # Load ilastik workflow
+#     self.workflow = ilastik.Workflow(self.ilastik_project)
+#     # Set input data
+#     self.input_slot = self.workflow.inputSlots[0]
+#     self.input_slot.setValue(self.input_data)
+#   def executefunc(self):
+#     # Run ilastik workflow
+#     self.workflow.run()
+#     # Get the output data
+#     self.output_data = self.workflow.outputSlots[0].value
+#     print(self.output_data) 
       
-    def write_data(self):
-     #save important data for analysis. 
-      
-class BattleZone(classify_observation):
-  def forward(self):
-    self.get_common_objects(self)
-    self.addtional_objects(self)
-    self.write_data(self)
+# # class BattleZone(classify_observation):
+# #   def forward(self):
+# #     self.get_common_objects(self)
+# #     self.addtional_objects(self)
+# #     self.write_data(self)
     
-  def additional_objects(self)
+# x = classify_observation()
+# x.executefunc()
   
+
+
+
+
+
+
+
+
