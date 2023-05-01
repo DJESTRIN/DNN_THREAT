73081c5582c579ff89e2ae1493727969ef8e60ac
anthonypoon12 <anthony@poons.us>
2023-05-01 13:53:07 -0400

    added basic classify

--- classify_observation.py
+++ classify_observation.py
@@ -1,33 +1,38 @@
-# Command should be python classify_observation.py PATH_TO_ILASTIK PROJECTFILE IMAGEFILES
-
-
+# Command should be python classify_observation.py
 import subprocess
 import os
 import sys
+import h5py
+import numpy as np
 
 # Get the current working directory
-current_dir = os.getcwd()
+class classify_observation:
+    def __init__(self,pathToIlastik, projectFilePath,exportFolder, inputFilePaths):
+        self.current_dir = os.getcwd()
+        os.chdir(pathToIlastik)
+        inputFilesPathsArr = []
+        for i in range(len(inputFilePaths)):
+            inputFilesPathsArr.append(self.current_dir + '\\' + inputFilePaths[i])
+        inputFilesPathsArr = ' '.join(inputFilesPathsArr)
+        os.system("ilastik.exe --headless --project={}\{} --export_source=\"Simple Segmentation\" --output_filename_format={}/{}/{}_{}.h5 {}".format(self.current_dir, projectFilePath, self.current_dir, exportFolder, '{nickname}', '{result_type}', inputFilesPathsArr))
+        os.chdir(self.current_dir)
 
-print("HEADS UP! This is assuming that you are using a Windows Computer and using Anaconda\n")
+    def getAllClassesInOutput(self):
+        outputFiles = os.listdir("Exports")
+        for file in outputFiles:
+            with h5py.File("Exports\{}".format(file), 'r') as f:
+                for dataset_name in f: #The dataset is 'exported_data', but just in case that doesn't work we use this
+                    dset = f[dataset_name]
+                    nparr = np.array(dset).flatten()
+                    with open("classifying.txt", "a") as textfile:
+                        textfile.write("Values for {}: {}\n".format(file, np.unique(nparr)))
+            os.remove("Exports\{}".format(file))
 
-pathToIlastik = sys.argv[1]
+# x = classify_observation("C:\Program Files\ilastik-1.4.0", "BattleZoneVersion0.ilp","Exports/" , ["BattleZoneImageFolder\BattleZone-v4Image14.png", "BattleZoneImageFolder\BattleZone-v4Image17.png", "BattleZoneImageFolder\BattleZone-v4Image31.png"])
+# x.getAllClassesInOutput()
 
-os.chdir(pathToIlastik)
+# classify_observation's init provides the outputs
+# Format of parameters:
+# (Path to Ilastik, Path to project file, path to export folder, path to inputs in arr form)
 
-projectFile = sys.argv[2]
-
-inputFiles = []
-
-print(os.getcwd() + "\n")
-
-for arg in sys.argv[3:]:
-    inputFiles.append(arg)
-
-for i in range(len(inputFiles)):
-    inputFiles[i] = current_dir + '\\' + inputFiles[i]
-
-inputFiles = ' '.join(inputFiles)
-
-print("ilastik.exe --headless --project={}\{} {}".format(current_dir, projectFile, inputFiles))
-
-os.system("ilastik.exe --headless --project={}\{} {}".format(current_dir, projectFile, inputFiles)) 
+# getAllClassesInOutput records the different classes in integer form on the textfile "classifying.txt"
---
+++ findingDataset.py
@@ -0,0 +1,31 @@
+import h5py
+import numpy as np
+
+# Open the h5 file in read mode
+# with h5py.File('SpaceInvaders-v4Image244_Simple Segmentation.h5', 'r') as f:
+with h5py.File('SpaceInvaders-v4Image279_Simple Segmentation.h5', 'r') as f:
+    for dataset_name in f: #The dataset is 'exported_data', but just in case that doesn't work we use this
+        dset = f[dataset_name]
+        nparr = np.array(dset).flatten()
+        print(np.unique(nparr))
+
+
+def whichClass(game, num):
+    if game == "SpaceInvaders":
+        match(num):
+            case 1:
+                return "Agent"
+            case 2:
+                return "Enemy"
+            case 3:
+                return "Background"
+            case 4:
+                return "Bullet"
+            case 5:
+                return "Reward"
+            case 6:
+                return "Wall"
+            case 7:
+                return "Death"
+            case 8:
+                return "Boost"
