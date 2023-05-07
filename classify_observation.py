33d78b5fe4d8e84da01cdf7973a111575605dc3b
anthonypoon12 <anthony@poons.us>
2023-05-07 13:07:42 -0400

    reorganized classify

--- classify_observation.py
+++ classify_observation.py
@@ -6,21 +6,29 @@ import h5py
 import numpy as np
 
 
-class classify_observation:
-    def __init__(self,pathToIlastik, projectFilePath,exportFolder):
+class Classify_observation:
+    # INITIALIZER
+    # pathToIlastik is the path to the Ilastik location
+    # projectFilepath is the filepath to the ilp file
+    # exportFolder is the folder you want to store
+        #if exportFolder doesn't exist, it will be created
+    def __init__(self,pathToIlastik, projectFilePath,exportFolder, exportTextFile):
         self.pathToIlastik = pathToIlastik
         self.projectFilePath = projectFilePath
         self.current_dir = os.getcwd()
         self.exportFolder = exportFolder
+        self.exportTextFile = exportTextFile
         # If the export folder doesn't exist, create it
         if not os.path.exists(self.exportFolder):
             os.mkdir(self.exportFolder)
 
-    # inputDir is a directory that contains the input files
-    def exportClassesFromDir(self, inputDir):
-        inputFiles = [inputDir + "\\" + f for f in os.listdir(inputDir) if f.endswith('.png')]
+    # produces the export h5 files and places them in the exportFolder based on input files in inputDirPath
+    # inputDirPath is a path to the directory that contains the input files
+    def exportClassesFromDir(self, inputDirPath):
+        inputFiles = [inputDirPath + "\\" + f for f in os.listdir(inputDirPath) if f.endswith('.png')]
         self.exportClassesFromFiles(inputFiles)
 
+    # produces the export h5 files and places them in the exportFolder based on an arr of inputFilePaths
     # inputFilePaths is an array of file paths
     def exportClassesFromFiles(self, inputFilePaths):
         # operation takes place at the Ilastik file location
@@ -31,7 +39,6 @@ class classify_observation:
         inputFilesPathsArr = ' '.join(inputFilesPathsArr)
         command = "ilastik.exe --headless --project={}\{} --export_source=\"Simple Segmentation\" --output_filename_format={}/{}/{}_{}.h5 {}".format(self.current_dir, self.projectFilePath, self.current_dir, self.exportFolder, '{nickname}', '{result_type}', inputFilesPathsArr)
         
-
         # Windows has a limit of about 8000 characters per command, so we divide the command in half if it is over
         if (len(command) > 8000):
             half_len = len(inputFilePaths) // 2
@@ -41,26 +48,18 @@ class classify_observation:
             # brings the os back to the original directory
             os.system(command)
             os.chdir(self.current_dir)
-            self.getAllClassesInOutput()
 
-    def getAllClassesInOutput(self):
-        outputFiles = [f for f in os.listdir(self.exportFolder) if f.endswith('.h5')]
-        for file in outputFiles:
-            with h5py.File("{}\{}".format(self.exportFolder, file), 'r') as f:
-                for dataset_name in f: #The dataset is 'exported_data', but just in case that doesn't work we use this
-                    dset = f[dataset_name]
-                    nparr = np.array(dset).flatten()
-                    with open("classifying.txt", "a") as textfile:
-                        textfile.write("Values for {}: {}\n".format(file, np.unique(nparr)))
-            os.remove("Exports\{}".format(file))
-
-
-# classify_observation's init provides the outputs
-# Format of parameters:
-# (Path to Ilastik, Path to project file, path to export folder)
-x = classify_observation("C:\Program Files\ilastik-1.4.0", "BattleZoneVersion0.ilp","Exports/")
-    
-# getAllClassesInOutput records the different classes in integer form on the textfile "classifying.txt"
-
-# x.exportClassesFromFiles(["BattleZoneImageFolder\BattleZone-v4Image1438.png", "BattleZoneImageFolder\BattleZone-v4Image1330.png", "BattleZoneImageFolder\BattleZone-v4Image1322.png"])
-x.exportClassesFromDir("BattleZoneImageFolder")
+    def displayAllClassesInOutputTextFile(self):
+        exportFolderFiles = os.listdir(self.exportFolder)
+        # outputFiles = [f for f in os.listdir(self.exportFolder) if f.endswith('.h5')]
+        for file in exportFolderFiles:
+            if file.endswith('.h5'):
+                with h5py.File("{}\{}".format(self.exportFolder, file), 'r') as f:
+                    for dataset_name in f: #The dataset is 'exported_data', but just in case that doesn't work we use this
+                        dset = f[dataset_name]
+                        nparr = np.array(dset).flatten()
+                        with open(self.exportTextFile, "a") as textfile:
+                            textfile.write("Values for {}: {}\n".format(file, np.unique(nparr)))
+                            os.remove("{}\{}".format(self.exportFolder,file))
+        with open(self.exportTextFile, "a") as textfile:
+            textfile.write("\n\n---------------\n\n")
